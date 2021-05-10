283bd6ddb7db93a3a1b804661c0ed4cee336f122
KillerKelvUK <kelvin.middleton@gmail.com>
2021-05-10 20:32:42 +0100

    Created Dockerfile, amended script to operate in bash container.  Container needs a volume mount to get to the script itself and logfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+# syntax=docker/dockerfile:1
+FROM bash:4.4
+CMD ["bash", "/config/net_checker.sh"]
--- net_checker.sh
+++ net_checker.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-interface=eth0
+interface="eth0"
 interval=$SECONDS
 exit_flag=0
 prev_second=$((SECONDS-1))
-loglocation=$PWD
-logfile=/net_checker.log
+loglocation="/config"
+logfile="net_checker.log"
 
 while [ $exit_flag -ne 1 ]; do                                                  #get a perpetual loop going
     if [ $SECONDS -gt $prev_second ]; then                                      #only consider doing something once a second
         prev_second=$SECONDS
         if [ $SECONDS -eq $interval ]; then                                     #do something on the configured interval
             if [ $(cat /sys/class/net/$interface/carrier) = 1 ]; then
-                echo "Online at" $(date)
+                echo "Online at" $(date) | tee -a $loglocation"/"$logfile
             elif [ $(cat /sys/class/net/$interface/carrier) = 0 ]; then
-                echo "****Offline at" $(date) "*****" >> $loglocation$logfile
+                echo "****Offline at" $(date) "*****" | tee -a $loglocation"/"$logfile
                 ping -4 -I $interface -c 1 8.8.8.8
             fi
             interval=$((SECONDS+3))
         fi
     fi
 done
-
